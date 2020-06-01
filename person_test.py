6f234ec73de72cc161ad1d84966531c431069cd8
KirigaiKabuto <tleugazy98@gmail.com>
2020-06-01 19:59:05 +0600

    Sdsds

--- person_test.py
+++ person_test.py
@@ -2,6 +2,7 @@ from person import Person
 
 p1 = Person(id=1)
 p1.set_info()
-
+print(p1.username)
+print(p1.password)
 #setInfo()->возвращать информацию об этом человеке из
 # базы и положить значения в текуший объект(ссылку) моего класса
