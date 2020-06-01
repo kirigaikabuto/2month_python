a83483789d8f19df3b4daaa1468ceaf71050ff1c
KirigaiKabuto <tleugazy98@gmail.com>
2020-06-01 20:23:31 +0600

    Sdsds

--- person_test1.py
+++ person_test1.py
@@ -3,3 +3,5 @@ p1 = Person(3,"sdsd","sdsd")
 p1.set_info()
 print(p1.username)
 print(p1.password)
+
+#если пользователя нет в базе но при этом вызывается set_info то тогда неоходимо вывести нет такого пользователя
